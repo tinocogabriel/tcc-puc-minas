7ab5c6a79ca6017cfa727f173433de3800e3c355
Gabriel Tinoco <tinocogabriel@gmail.com>
2020-05-28 22:22:55 -0300

    ajustes deploy

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,9 +1,11 @@
+require('dotenv/config');
+
 module.exports = {
   dialect: "postgres",
-  host: "localhost",
-  username: "tinoco",
-  password: "123456789",
-  database: "muscles_gym",
+  host: process.env.DB_HOST,//"localhost",
+  username: process.env.DB_USERNAME,//"tinoco",
+  password: process.env.DB_PASSWORD,//"123456789",
+  database: process.env.DB_DATABASE,//"muscles_gym",
   define: {
     timestamps: false,
     underscored: true,
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
+import 'dotenv/config';
+
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333',
+    baseURL: process.env.BASE_URL,
 });
 
 export default api;
